4229fc0c9d9349a67cae2253061acd696efc2f1e
Coderwsq <9699507+wsq__666@user.noreply.gitee.com>
2023-01-26 17:29:59 +0800

    MVC和MVVM

---
+++ 13.MVC和MVVM/index.ts
@@ -0,0 +1,16 @@
+/**
+ * MVC
+ * 1.M: Model数据结构
+ * 2.V: View 视图
+ * 3.C: Controller 业务逻辑控制
+ * 4.流程：View发送指令到 Controller, Controller执行业务逻辑修改 Model, Model变化重新渲染 View
+ */
+
+/**
+ * MVVM (M V VM ) 对比Vue框架设计架构
+ * 1.M: Model数据结构 (Vue-data)
+ * 2.V: View 视图 (Vue-template)
+ * 3.VM: ViewModel (Vue其他核心功能 负责连接View和Model)
+ */
+
+export { }
